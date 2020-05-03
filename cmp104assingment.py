b21fe7133e722efd723e58cd255be53bad3bbb51
Gabriel Temsten <57184013+gabrieltemtsen@users.noreply.github.com>
2020-05-03 16:32:29 +0000

    Update cmp104assingment.py

--- cmp104assingment.py
+++ cmp104assingment.py
@@ -2,37 +2,37 @@
 
 #To calculate the area of a square
 print("Calculate the area of a Square (Formula :a x a:)")\n
-a = int(input("Enter the Value of a: "))
+a = float(input("Enter the Value of a: "))
 b = a * a
 print("Hello the result is ",b)
 
 #To calculate the area of a Rectangle
 print("Calculate the area of a Rectangle (Formula :l x b:)")\n
-l = int(input("Enter the Value of l: "))
-b = int(input("Enter the Value of b: "))
+l = float(input("Enter the Value of l: "))
+b = float(input("Enter the Value of b: "))
 r = l * b
 print("Hello the result is ",r)
 
 
 #To calculate the area of a Triangle
 print("Calculate the area of a Triangle (Formula :1/2 x b x h:)")\n
-b = int(input("Enter the Value of b: "))
-h = int(input("Enter the Value of h: "))
+b = float(input("Enter the Value of b: "))
+h = float(input("Enter the Value of h: "))
 r = 1 / 2 * (b * h)
 print("Hello the result is ",r)
 
 #To calculate the area of a Trapezoid
 print("Calculate the area of a Triangle (Formula:1/2 x (b1 + b2)xh:)")\n
-b1 = int(input("Enter the Value of b1: "))
-b2= int(input("Enter the Value of b2: "))
-h= int(input("Enter the Value of h: "))
+b1 = float(input("Enter the Value of b1: "))
+b2= float(input("Enter the Value of b2: "))
+h= float(input("Enter the Value of h: "))
 r = 1 / 2 * (b1 + b2) * h
 print("Hello the result is ",r)
 
 
 #To calculate the area of a Circle
 print("Calculate the area of a Cicle (Formula :PIxrxr:)")\n
-rd = int(input("Enter the Value of r: "))
+rd = float(input("Enter the Value of r: "))
 pi = 22/7
 s = rd * rd
 r = pi * s
@@ -40,30 +40,30 @@ print("Hello the result is ",r)
 
 #To calculate Surcface area of a Cube
 print("Calculate Surcface area of a Cube (Formula :2xPIxr:)")\n
-rd = int(input("Enter the Value of r: "))
+rd = float(input("Enter the Value of r: "))
 pi = 22/7
 r = 2 * pi * rd
 print("Hello the result is ",r)
 
 #To calculate the Surcface area of a Cube
 print("Calculate the Surcface area of a Cube (Formula :6xaxa:)")\n
-a = int(input("Enter the Value of a: "))
+a = float(input("Enter the Value of a: "))
 s = a * a 
 r = 6 * s
 print("Hello the result is ",r)
 
 #To calculate the curved surface area of a cylinder 
 print("Calculate curved surface area of a cylinder (Formula :2PIxrx(r+h):)")\n
-rd = int(input("Enter the Value of r: "))
-h = int(input("Enter the Value of h: "))
+rd = float(input("Enter the Value of r: "))
+h = float(input("Enter the Value of h: "))
 pi = 22/7
 r = 2 * pi * rd * ( rd + h)
 print("Hello the result is ",r)
 
 #To calculate the Volume of a Cyinder
 print("Calculate the Volume of a Cyinder(Formula :PIx(rxr)*h:)")\n
-rd = int(input("Enter the Value of r: "))
-h = int(input("Enter the Value of h: "))
+rd = float(input("Enter the Value of r: "))
+h = float(input("Enter the Value of h: "))
 pi = 22/7
 s = rd * rd
 r = pi * s * h
@@ -71,38 +71,38 @@ print("Hello the result is ",r)
 
 #To calculate the Acceleration Formula
 print("Calculate the  Acceleration Formula(Formula :V-U/t:)")\n
-v = int(input("Enter the Value of v: "))
-u = int(input("Enter the Value of u: "))
-t = int(input("Enter the Value of t: "))
+v = float(input("Enter the Value of v: "))
+u = float(input("Enter the Value of u: "))
+t = float(input("Enter the Value of t: "))
 r = (v - u) / t
 print("Hello the result is ",r)
 
 #To calculate the Density Formula
 print("Calculate the Density Formula(Formula :M/V:)")\n
-m = int(input("Enter the Value of m: "))
-v = int(input("Enter the Value of v: "))
+m = float(input("Enter the Value of m: "))
+v = float(input("Enter the Value of v: "))
 r = m / v
 print("Hello the result is ",r)
 
 #To calculate the Formula for pressure
 print("Calculate the Volume of a Cyinder(Formula :F/A:)")\n
-f = int(input("Enter the Value of f: "))
-a = int(input("Enter the Value of a: "))
+f = float(input("Enter the Value of f: "))
+a = float(input("Enter the Value of a: "))
 r = f / a
 print("Hello the result is ",r)
 
 #To calculate the formula for kinetic Energy
 print("Calculate the formula for kinetic Energy(Formula :1/2M(vxv):)")\n
-m = int(input("Enter the Value of m: "))
-v = int(input("Enter the Value of v: "))
+m = float(input("Enter the Value of m: "))
+v = float(input("Enter the Value of v: "))
 s = v * v 
 r = 1/2 * m * s
 print("Hello the result is ",r)
 
 #To calculate the formula for voltage
 print("Calculate the formula for voltage(Formula :IR:)")\n
-i = int(input("Enter the Value of i: "))
-r = int(input("Enter the Value of r: "))
+i = float(input("Enter the Value of i: "))
+r = float(input("Enter the Value of r: "))
 ro = i * r
 print("Hello the result is ",ro)
 
